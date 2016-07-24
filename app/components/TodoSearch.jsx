c8f830afd3e7c1c4ba0d93eaa82d1e050898876f
Konrad <jarson@me.com>
2016-07-24 03:05:50 +0100

    Filtering todos

---
+++ app/components/TodoSearch.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var {FormControl, Checkbox} = require('react-bootstrap');
+
+var TodoSearch = React.createClass({
+	handleSearch: function() {
+		var showCompleted = ReactDOM.findDOMNode(this.refs.showCompleted).checked;
+		var searchText = ReactDOM.findDOMNode(this.refs.searchText).value;
+		
+		this.props.onSearch(showCompleted, searchText);
+	},
+	render: function() {
+		return (
+		<div>
+			<div>
+				<FormControl ref="searchText" type="search" placeholder="Search Todos" onChange={this.handleSearch}/>
+			</div>
+			<div className="togglebutton">
+				<label>
+					<input type="checkbox" ref="showCompleted" onChange={this.handleSearch}/>
+					Show completed todos
+				</label>
+			</div>
+		</div>
+		);
+	}
+});
+
+module.exports = TodoSearch;
