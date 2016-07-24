95c94a8e6e5976f258e49ef1ac87d24a51dd5a3f
Konrad <jarson@me.com>
2016-07-24 02:48:15 +0100

    Test suite

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var {FormControl, Button} = require('react-bootstrap');
+
+var AddTodo = React.createClass({
+
+
+	onSubmit: function(e) {
+		e.preventDefault();
+		var text = ReactDOM.findDOMNode(this.refs.todoText).value;
+
+		if (text.length > 0) {
+			this.props.onAddTodo(text);
+			ReactDOM.findDOMNode(this.refs.todoText).value = '';
+		} else {
+			ReactDOM.findDOMNode(this.refs.todoText).focus();
+		}
+	},
+	render: function() {
+		return (
+			<div>
+				<form onSubmit={this.onSubmit}>
+				<FormControl ref="todoText"/>
+				<Button className="btn-block" type="submit" bsStyle="info">Add</Button>
+				</form>
+			</div>
+		);
+	}
+});
+
+module.exports = AddTodo;
---
+++ app/tests/components/AddTodo.test.jsx
@@ -0,0 +1,36 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var AddTodo = require('AddTodo');
+
+describe('AddTodo', () => {
+	it ('should exist', () => {
+		expect(AddTodo).toExist();
+	});
+
+	it('should call onAddTodo prop with valid data', () => {
+		var spy = expect.createSpy();
+		var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+		var $el = $(ReactDOM.findDOMNode(addTodo));
+
+		ReactDOM.findDOMNode(addTodo.refs.todoText).value = 'Check mail';
+		TestUtils.Simulate.submit($el.find('form')[0]);
+
+		expect(spy).toHaveBeenCalledWith('Check mail');
+
+	});
+
+	it ('shouldn\'t call onAddTodo with invalid data', () => {
+		var spy = expect.createSpy();
+		var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+		var $el = $(ReactDOM.findDOMNode(addTodo));
+
+		ReactDOM.findDOMNode(addTodo.refs.todoText).value = '';
+		TestUtils.Simulate.submit($el.find('form')[0]);
+
+		expect(spy).toNotHaveBeenCalled();
+	});
+});
